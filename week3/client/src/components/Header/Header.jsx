fef2f873b5f21df72b6c3b2e2170f002f616612c
Syeda Aashna Fathima <aashna.fathia.23@gmail.com>
2023-10-28 14:49:03 +0530

    allweek

---
+++ week3/client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from "react-ions/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import Search from "./Search/Search";
+import Cart from "./Cart/Cart";
+import { Context } from "../../utils/context";
+
+import "./Header.scss";
+
+const Header = () => {
+    return <div>Header</div>;
+};
+
+export default Header;
